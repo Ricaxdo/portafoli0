9f081d0c7bd2e96f599eb2b1e2ec2fadcee411a9
Ricardo Castro <ricardo.caroj99@gmail.com>
2025-10-26 21:51:14 -0600

    sd

--- next.config.mjs
+++ next.config.mjs
@@ -1,16 +1,11 @@
-// next.config.mjs
 /** @type {import('next').NextConfig} */
 const isProd = process.env.NODE_ENV === "production";
 
 const nextConfig = {
-  output: "export", // <- genera /out para GitHub Pages
-  images: { unoptimized: true }, // <- obligatorio en export
-  trailingSlash: true, // <- crea /ruta/index.html (evita 404 en GH Pages)
-  basePath: isProd ? "/portafoli0" : "", // <- NOMBRE EXACTO DEL REPO
+  output: "export",
+  images: { unoptimized: true },
+  trailingSlash: true,
+  basePath: isProd ? "/portafoli0" : "",
   assetPrefix: isProd ? "/portafoli0/" : "",
-
-  // Opcional: si estás en pleno desarrollo con tipos
-  typescript: { ignoreBuildErrors: true },
 };
-
 export default nextConfig;
